21e0df9a33611f688ab6ccc1af4dc12071d9ff7c
kuro <spider.kom@gmail.com>
2018-04-24 18:33:00 +0300

    6

---
+++ story.js
@@ -0,0 +1,9 @@
+var story = 
+`A young man stands in his bedroom. 
+
+It just so happens that today, the 13th of April, 2009, is this young man's birthday. 
+
+Though it was thirteen years ago he was given life, it is only today he will be given a name!
+
+What will the name of this young man be?
+`;
